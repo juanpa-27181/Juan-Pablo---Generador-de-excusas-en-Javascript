dee6299bc1c10de8de7d15b3d0fef73e9f5ea44f
juanpa-27181 <74931576+juanpa-27181@users.noreply.github.com>
2025-08-21 22:11:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-function generateExcuse() {
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
-  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch'];
+window.onload = function () {
 
-  let randomWho = who[Math.floor(Math.random() * who.length)];
-  let randomAction = action[Math.floor(Math.random() * action.length)];
-  let randomWhat = what[Math.floor(Math.random() * what.length)];
-  let randomWhen = when[Math.floor(Math.random() * when.length)];
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
-  document.getElementById("excuse").innerHTML = excuse;
+function getRandomElement(array){
+    return array[Math.floor(Math.random() * array.length)];
 }
+
+let excuse = 
+    getRandomElement(who) + " " +
+    getRandomElement(action) + " " +
+    getRandomElement(what) + " " + 
+    getRandomElement(when) + "." ;
+console.log(excuse);
+
+document.getElementById("excuse").innerHTML = excuse;
+};
